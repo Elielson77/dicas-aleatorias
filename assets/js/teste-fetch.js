61d0fdccf5acc00b1bc3a758d229a5d606d5291b
Elielson Melo <88008375+Elielson77@users.noreply.github.com>
2022-08-12 19:29:08 -0300

    remove function translate

--- assets/js/teste-fetch.js
+++ assets/js/teste-fetch.js
@@ -5,27 +5,27 @@ const obj = {
   url: 'https://api.adviceslip.com/advice'
 }
 
-function traduzir(texto) {
-  const encodedParams = new URLSearchParams();
-  encodedParams.append("q", texto);
-  encodedParams.append("target", "pt");
+// function traduzir(texto) {
+//   const encodedParams = new URLSearchParams();
+//   encodedParams.append("q", texto);
+//   encodedParams.append("target", "pt");
 
-  const options = {
-    method: 'POST',
-    headers: {
-      'content-type': 'application/x-www-form-urlencoded',
-      'Accept-Encoding': 'application/gzip',
-      'X-RapidAPI-Key': '9dbdc28de3msh3cc643146f3d5f1p198e01jsnf08966e627ca',
-      'X-RapidAPI-Host': 'google-translate1.p.rapidapi.com'
-    },
-    body: encodedParams
-  };
+//   const options = {
+//     method: 'POST',
+//     headers: {
+//       'content-type': 'application/x-www-form-urlencoded',
+//       'Accept-Encoding': 'application/gzip',
+//       'X-RapidAPI-Key': '9dbdc28de3msh3cc643146f3d5f1p198e01jsnf08966e627ca',
+//       'X-RapidAPI-Host': 'google-translate1.p.rapidapi.com'
+//     },
+//     body: encodedParams
+//   };
 
-  fetch('https://google-translate1.p.rapidapi.com/language/translate/v2', options)
-    .then(response => response.json())
-    .then(response => exibeResult(response.data.translations[0].translatedText))
-    .catch(err => console.log(err));
-}
+//   fetch('https://google-translate1.p.rapidapi.com/language/translate/v2', options)
+//     .then(response => response.json())
+//     .then(response => exibeResult(response.data.translations[0].translatedText))
+//     .catch(err => console.log(err));
+// }
 
 const request = () => {
   fetch(obj.url)
@@ -33,7 +33,7 @@ const request = () => {
       return response.text()
     })
     .then(text => {
-      traduzir(JSON.parse(text).slip.advice);
+      exibeResult(JSON.parse(text).slip.advice);
     })
     .catch(error => {
       console.log(error)
